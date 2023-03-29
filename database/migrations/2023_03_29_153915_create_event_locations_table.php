548c2b2615021d20260eb9276b95ea4acede120c
Valentin_Peintner <valentin.peintner@gmx.at>
2023-03-29 17:51:57 +0200

    update+

---
+++ database/migrations/2023_03_29_153915_create_event_locations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event-locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('phone_number');
+            $table->string('email');
+            $table->string('website_url');
+            $table->string('owner');
+            $table->boolean('bookable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event-locations');
+    }
+}
